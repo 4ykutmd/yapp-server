7d02a7b6f4a1f63fb29df6a1c154de16956395d3
Aykut Mehmet Doğan <aykutfx2210@gmail.com>
2024-10-29 20:08:54 +0300

    yuklenen gorseli gemini'a yorumlatma

--- index.js
+++ index.js
@@ -28,12 +28,57 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GENAI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
+const { GoogleAIFileManager } = require("@google/generative-ai/server");
+const fileManager = new GoogleAIFileManager(process.env.GENAI_API_KEY);
+
 app.get('/api/soru-sor', upload.single('image') , async (req, res) => {
-    const textinput = req.query.soru;
+    const manualPrompt = req.query.soru;
+    const type = req.query.type || '1';
+    //1: soru cozdur, 2: sorudaki konuyu anlatmasini iste
+
+    const prompt = type === '1' ? 'Görseldeki soruyu çözer misin?' : 'Sorudaki konuyu anlatır mısın?';
+
+    if (!manualPrompt) {
+        const uploadResponse = await fileManager.uploadFile(req.file.path, {
+            mimeType: req.file.mimetype,
+            displayName: req.file.filename,
+        });
+        console.log(`Uploaded file ${uploadResponse.file.displayName} as: ${uploadResponse.file.uri}`);
+
+        //const getResponse = await fileManager.getFile(uploadResponse.file.name);
+
+        //console.log(`Retrieved file ${getResponse.displayName} as ${getResponse.uri}`);
+
+        const result = await model.generateContent([
+            {
+                fileData: {
+                    mimeType: uploadResponse.file.mimeType,
+                    fileUri: uploadResponse.file.uri
+                }
+            },
+            {
+                text: prompt
+            }
+        ]);
+
+        const final_result = result.response.text();
+
+        res.status(200).json({ cevap: final_result });
+    } else {
+
+        const result = await model.generateContent([
+            {
+                text: manualPrompt
+            },
+        ])
+        const final_result = result.response.text();
+
+        res.status(200).json({ cevap: final_result });
+    }
 
     console.log(req.file);
     //console.log(!textinput);
-    res.status(200).json({ message: 'Soru alındı' });
+    
 })
 
 app.listen(port, () => {
