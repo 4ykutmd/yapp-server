8c35a15735b05573872644c2790668abefb17640
Aykut Mehmet Doğan <63978038+aykutmehmetd@users.noreply.github.com>
2024-11-02 16:05:54 +0300

    pardon emir

--- chatPage.js
+++ chatPage.js
@@ -1,2 +1,17 @@
 const express = require('express');
 const router = express.Router();
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI(process.env.GENAI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+router.get('/', (req, res) => {
+    const manualPrompt = req.query.mesaj;
+    const hazirPrompt = req.query.promptType || '1';
+
+    if (hazirPrompt === '1' && !manualPrompt) {
+
+    }
+});
+
+module.exports = router;
